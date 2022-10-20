12e0dad0e82852169faba24ffefb6bf3101eb535
Lautaro Barrera <lautarobarrera12@gmail.com>
2022-10-20 01:43:17 -0300

    estructura de carpeta creada

---
+++ elebe-blocks.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       Elebe Blocks
+ * Plugin URI:        https://github.com/lautarobarrera10/elebe-blocks
+ * Description:       Plugin de bloques desarrollados por Elebe
+ * Version:           1.0
+ * Requires at least: 5.8
+ * Requires PHP:      7.4
+ * Author:            Lautaro Barrera
+ * Author URI:        https://github.com/lautarobarrera10
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       elebe
+*/
